80fcd52449eefc98466421e80c5a93575c0a115e
ndtduy <ndtduy061003@gmail.com>
2022-12-17 12:45:08 +0700

    HW-6

---
+++ HW-6/RadixSort/main.cpp
@@ -0,0 +1,37 @@
+#include "LinkedList.h"
+
+int main() {
+    ifstream inp;
+    inp.open("input.txt", ios::in);
+    if (!inp.good()) {
+        return 0;
+    }
+
+    NRef head = nullptr;
+    NRef tail = nullptr;
+
+    int k, n, max_val = 0;
+    inp >> k >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        inp >> x;
+        addTail(head, tail, x);
+        if (x > max_val) {
+            max_val = x;
+        }
+    }
+
+    inp.close();
+    
+    int d = 0;
+    while (max_val > 0) {
+        max_val /= 10;
+        d++;
+    }
+
+    radixSort(head, tail, k, d);
+    for (NRef p = head; p != nullptr; p = p->next) {
+        cout << p->key << ' ';
+    }
+}
